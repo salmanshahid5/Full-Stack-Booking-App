3b14c3f941827de1af2210695fb99f084c91aeae
salmanshahid5 <salmanshahid0333@gmail.com>
2025-01-19 16:42:29 +0500

    hotel controller create

---
+++ api/controllers/hotel.js
@@ -0,0 +1,56 @@
+import Hotel from "../models/hotel.js";
+
+// create
+export const createHotel = async (req, res, next) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// update
+export const updateHotel = async (req, res, next) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// delete
+export const deleteHotel = async (req, res, next) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted.");
+  } catch (err) {
+    next(err);
+  }
+};
+
+// getHotel
+export const getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// getAll
+export const getHotels = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.find(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    next(err);
+  }
+};
